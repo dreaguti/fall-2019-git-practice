66091e7b2c609de83b2d91c5facc3c66189ce5ba
Andrea Gutierrez <drea.guti@gmail.com>
2020-04-29 14:39:25 -0400

    comment

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -2,7 +2,11 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world.");
         System.out.println("Change One.");
+<<<<<<< Updated upstream
         System.out.println("Change Two."); //github comment github
+=======
+        System.out.println("Change Two."); //git comment
+>>>>>>> Stashed changes
         System.out.println("Change Three.");
     }
 }
